eadf186df98a58212b7360888f7dc67ca3e34cdc
Hari Krishna Podaralla <pharikrishna@outlook.com>
2019-07-07 19:41:11 +0530

    this

--- CoreJava/src/ExamplesCodeTesting/MyThisExample.java
+++ CoreJava/src/ExamplesCodeTesting/MyThisExample.java
@@ -4,18 +4,78 @@ public class MyThisExample {
 	
 	int a=22;
 	int b=60;
-
-	public  void main(String[] args) {
+	
+	 MyThisExample() {
+		// TODO Auto-generated constructor stub
+		 // this(45,54); //intilization done in parametarised construtor
+		   a= 79; b= 78;
+		 System.out.println("this is defalut constructor");
+	}
+	 
+	 MyThisExample method()
+	 
+	 {
+		 System.out.println("this is from method in MyThisExample");
+	
+		return this;
+		
+		
+	 }
+	
+	MyThisExample(int localref , int  localref2)
+	{
+		this.a=localref;
+		this.b=localref2;
+		System.out.println("this is from constructor with 2 parameters");
+		
+	}
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=21;
-		int b=16;
 		
-		System.out.println(this.a + a);
-		System.out.println(this.b + b);
-		System.out.println("hi this key word");
 		
+		MyThisExample objectref  = new MyThisExample(66,99 );
+		objectref.method1();
+		System.out.println("==============");
+		System.out.println("this is object 2");
+		MyThisExample objectreftwo  = new MyThisExample( );
+		objectreftwo.method1();
+		System.out.println("==============");
+		System.out.println("this is object 3");
+		MyThisExample objectrefThree = new MyThisExample();
+		objectrefThree.method().method1();
+		System.out.println("==============");
+		System.out.println("this is object 4");
+		MyThisExample objectrefFour = new MyThisExample();
+		objectrefFour.method3();
+		
+		
+		
+		
+		int  a =1;
+		int b=2;
 		
 
 	}
 
+	void method1()
+	{
+		int a=21;
+		int b=16;
+		System.out.println(this.a+ "                        " + a);
+		System.out.println(this.b +  "                       " +b);
+		System.out.println("global values -----local method values");
+		
+		
+	}
+	
+	void method2(MyThisExample objectref)
+	{
+		System.out.println("this is method2 from MyThisExample ");
+		System.out.println(a +"    =====    "+ b);
+		
+	}
+	void method3()
+	{
+		method2(this);
+	}
 }
--- CoreJava/src/ExamplesCodeTesting/MyThisTest.java
+++ CoreJava/src/ExamplesCodeTesting/MyThisTest.java
@@ -6,14 +6,14 @@ public class MyThisTest {
 	int b=60;
 
 
-	public  void main(String[] args) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		int a=21;
 		int b=16;
 		
-		System.out.println(this.a + a);
-		System.out.println(this.b + b);
+		/*System.out.println(this.a + a);
+		System.out.println(this.b + b);*/
 		System.out.println("hi this key word");
 
 	}
